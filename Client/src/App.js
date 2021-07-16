8becd1bb0fc023c2718e837f226848c01417cb8d
Cooperator0419 <cooperator0419@gmail.com>
2021-07-15 19:17:34 -0700

    feat : google-login

--- Client/src/App.js
+++ Client/src/App.js
@@ -1,10 +1,39 @@
 import React, { useEffect } from "react";
+import { GoogleLogin, useGoogleLogin } from "react-google-login";
 
 function App() {
+  const onSuccess = (res) => {
+    // console.log(res);
+    console.log(res.Ys.It, res.Ys.Ve);
+  };
+
+  const onFailure = (res) => {
+    console.log(res);
+  };
+
   return (
     <div className="App">
       <center>
-        <button onClick={signInWithGoogle}>Sign in with google</button>
+        <GoogleLogin
+          clientId={
+            "1040122098766-np12c1kq29d6l16cmo8o5gk5ptrv98go.apps.googleusercontent.com"
+          }
+          render={(renderProps) => (
+            <button
+              onClick={renderProps.onClick}
+              disabled={renderProps.disabled}
+            >
+              Google
+            </button>
+          )}
+          buttonText="Login"
+          onSuccess={onSuccess}
+          uxMode={"redirect"}
+          onFailure={onFailure}
+          RedirectURL={"postmessage"}
+          cookiePolicy={"single_host_origin"}
+          // responseType="code"
+        />
       </center>
     </div>
   );
