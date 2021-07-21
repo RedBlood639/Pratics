426291819348156f84a260f61dc784aac62278a7
Cooperator0419 <cooperator0419@gmail.com>
2021-07-20 19:18:48 -0700

    ASD12879ADS

--- src/app/BasePage.js
+++ src/app/BasePage.js
@@ -2,6 +2,7 @@ import React, { Suspense, lazy } from "react";
 import { Redirect, Switch, Route } from "react-router-dom";
 import { LayoutSplashScreen, ContentRoute } from "../_metronic/layout";
 import { MyPage } from "./pages/MyPage";
+import { DashboardPage } from "./pages/DashboardPage";
 
 const ECommercePage = lazy(() =>
   import("./modules/ECommerce/pages/eCommercePage")
@@ -11,8 +12,13 @@ export default function BasePage() {
   return (
     <Suspense fallback={<LayoutSplashScreen />}>
       <Switch>
-        <Redirect exact from="/" to="/my-page" />
+        {
+          /* Redirect from root URL to /dashboard. */
+          <Redirect exact from="/" to="/dashboard" />
+        }
+        <ContentRoute path="/dashboard" component={DashboardPage} />
         <ContentRoute path="/my-page" component={MyPage} />
+
         <Route path="/e-commerce" component={ECommercePage} />
         <Redirect to="error/error-v1" />
       </Switch>
