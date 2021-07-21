157605f2b7f269a7ebf558f495719f2d9b014247
Cooperator0419 <cooperator0419@gmail.com>
2021-07-20 19:17:51 -0700

    S32A4AWE1

--- src/app/BasePage.js
+++ src/app/BasePage.js
@@ -2,7 +2,6 @@ import React, { Suspense, lazy } from "react";
 import { Redirect, Switch, Route } from "react-router-dom";
 import { LayoutSplashScreen, ContentRoute } from "../_metronic/layout";
 import { MyPage } from "./pages/MyPage";
-import { DashboardPage } from "./pages/DashboardPage";
 
 const ECommercePage = lazy(() =>
   import("./modules/ECommerce/pages/eCommercePage")
@@ -12,13 +11,8 @@ export default function BasePage() {
   return (
     <Suspense fallback={<LayoutSplashScreen />}>
       <Switch>
-        {
-          /* Redirect from root URL to /dashboard. */
-          <Redirect exact from="/" to="/dashboard" />
-        }
-        <ContentRoute path="/dashboard" component={DashboardPage} />
+        <Redirect exact from="/" to="/my-page" />
         <ContentRoute path="/my-page" component={MyPage} />
-
         <Route path="/e-commerce" component={ECommercePage} />
         <Redirect to="error/error-v1" />
       </Switch>
