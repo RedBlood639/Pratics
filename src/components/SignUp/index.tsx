13ce734dcaec1536d6153e581e28ed339b086173
Cooperator0419 <cooperator0419@gmail.com>
2021-07-22 13:53:03 -0700

    SignIn Page

---
+++ src/components/SignUp/index.tsx
@@ -0,0 +1,46 @@
+import React from "react";
+import "./style.scss";
+
+const SignUp: React.FC<{ history?: any; match?: any }> = ({ history }) => {
+  return (
+    <React.Fragment>
+      <form className="form-content">
+        <div>
+          <p className="bg-label color-primary font-weight-700">Sign Up</p>
+          <span className="sm-label color-gray-2">
+            Enter your details to create your account?
+          </span>
+        </div>
+        <div>
+          <p className="sm-label">Email</p>{" "}
+          <input
+            type="text"
+            name="email"
+            placeholder="please input your email"
+          />
+          <p className="label color-danger">Username is required</p>
+        </div>
+        <div>
+          <div className="forget">
+            <span className="sm-label">Password</span>
+            <a href="#" className="sm-label color-blue-1 font-weight-800">
+              Forgot Password ?
+            </a>
+          </div>
+          <p></p>
+          <input
+            type="password"
+            name="password"
+            placeholder="please input your password"
+          />
+          <p className="label color-danger">Password is required</p>
+        </div>
+        <div>
+          <button className="font-weight-600 sm-label">Sign In</button>
+        </div>
+      </form>
+    </React.Fragment>
+  );
+};
+
+export default SignUp;
--- src/pages/AdminSetup/Home/index.tsx
+++ src/pages/AdminSetup/Home/index.tsx
@@ -1,18 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
+import SignIn from "../../../components/SignIn/Iindex";
+import SignUp from "../../../components/SignUp";
 import { LogoBig } from "../../../assets/icons/LogoBig";
+
 import "./style.scss";
 
 const HomePage: React.FC<{ history?: any; match?: any }> = ({ history }) => {
   return (
     <div className="home-container">
-      <div className="aside-display">
-        <div className="logo-header">
-          <LogoBig />
+      <div className="left-container"></div>
+      <div className="right-container">
+        <div className="form-container">
+          {/* <SignIn /> */}
+          <SignUp />
         </div>
-        <div className="logo-content">123</div>
-        <div className="logo-footer">123</div>
       </div>
-      <div className="content-display"></div>
     </div>
   );
 };
