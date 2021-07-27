b7bb7e7efd32ed24104e5510a06a820e544150ab
Cooperator0419 <cooperator0419@gmail.com>
2021-07-27 14:50:14 -0700

    QWE8712E9Q

--- src/pages/DashBoard/UserManage/AddUser/index.tsx
+++ src/pages/DashBoard/UserManage/AddUser/index.tsx
@@ -7,48 +7,59 @@ const AddUser: React.FC = () => {
   const sections = [
     { title: "Account" },
     { title: "Payment" },
-    { title: "Review" },
+    { title: "Avatar" },
   ];
 
   const next = () => setCurrentPage((prev) => prev + 1);
   const prev = () => setCurrentPage((prev) => prev - 1);
 
   return (
-    <div className="form-containter">
-      <div className="form-content">
-        <div className="stepper">
-          <Stepper
-            steps={sections}
-            activeStep={currentPage}
-            completeColor="#10889d"
-            completeBarColor="#10889d"
-          />
-        </div>
-        <div className="stepper-content">
-          {currentPage === 1 && (
-            <>
-              <button onClick={next}>Next</button>
-            </>
-          )}
+    <div className="form-container">
+      <div className="stepper">
+        <Stepper
+          steps={sections}
+          activeStep={currentPage}
+          completeColor="#10889d"
+          completeBarColor="#10889d"
+        />
+      </div>
 
-          {currentPage === 2 && (
-            <>
-              <div style={{ display: "flex", justifyContent: "space-between" }}>
-                <button onClick={prev}>Back</button>
-                <button onClick={next}>Next</button>
-              </div>
-            </>
-          )}
+      <div className="stepper-content">
+        {currentPage === 1 && (
+          <div className="account-container">
+            <div className="account-header">{"SignUp User"}</div>
+            <br />
+            <div className="account-content">
+              <input type="text" placeholder="Input user's name" />
+              <input type="text" placeholder="Input user's password" />
+              <input type="text" placeholder="Confirm user's password" />
+            </div>
+            <br />
+            <div className="account-footer">
+              <button onClick={next} role="next">
+                Next
+              </button>
+            </div>
+          </div>
+        )}
 
-          {currentPage === 3 && (
-            <>
-              <div style={{ display: "flex", justifyContent: "space-between" }}>
-                <button onClick={prev}>Back</button>
-                <button>Finish</button>
-              </div>
-            </>
-          )}
-        </div>
+        {currentPage === 2 && (
+          <div>
+            <button onClick={prev} role="before">
+              Back
+            </button>
+            <button onClick={next} role="next">
+              Next
+            </button>
+          </div>
+        )}
+
+        {currentPage === 3 && (
+          <div>
+            <button onClick={prev}>Back</button>
+            <button>Finish</button>
+          </div>
+        )}
       </div>
     </div>
   );
